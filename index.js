ce6d3dd4bdf64f7a867e2c67e35f7503f290ad7f
Bhrugu <bhrugupadhi@gmail.com>
2023-02-24 22:57:28 +0530

    add

--- index.js
+++ index.js
@@ -1,14 +1,60 @@
+
+const bcrypt = require("bcrypt/bcrypt")
 const express = require("express");
-require("dotenv").config()
+const jwt = require("jsonwebtoken");
+
+const UserModel = require("./models/User.model")
+const flightsRouter = require("./routes/Flights.routes");
+const connection = require("./configs/db");
 
 const app = express();
-
+app.use(express.json());
+require("dotenv").config()
 const PORT = process.env.PORT || 3000
-
 app.get("/",(req,res)=>{
-    res.send("Hello")
+    res.send("Hello World")
 })
 
+app.post("/api/register",async(req,res) =>{
+    const {email,password,name} = req.body;
+    await bcrypt.hash(password,6,function(err,hash){
+        if(err){
+            return res.send("Signup failure")
+        }
+        const user = new UserModel({email,password:hash,name})
+        user.save()
+        return res.send("Signup successful")
+    })
+})
+app.post("/api/login",async(req,res) =>{
+    const {email,password} = req.body;
+    const user = await UserModel.findOne({email})
+    if(!user){
+        return res.send("Check credentials")
+    }
+    const haspassword = user.password
+    await bcrypt.compare(password,haspassword,(err,result) =>{
+        if(err) {
+            return res.send("error")
+        }
+        if(result==true){
+            const token = jwt.sign({email:user.email,_id:user._id},process.env.jwt_secret_key)
+            return res.send({message:"Login success",token:token,userId:user._id})
+        }
+        else{
+            return res.send("Error Please Check")
+        }
+    })
+})
+
+app.use("/api/flights",flightsRouter)
+
 app.listen(PORT,async () =>{
-    console.log(`listening on port${PORT}`)
+    try{
+        await connection
+        console.log("Connect success to db")
+    }
+    catch(err){
+        console.log("connect failure")
+    }
 })
