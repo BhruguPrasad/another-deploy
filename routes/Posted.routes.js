bd6101ff08fddb01c409b6020781ef0e7b0773f5
Bhrugu Prasad <98230777+BhruguPrasad@users.noreply.github.com>
2023-02-28 15:19:51 +0530

    Update Posted.routes.js

--- routes/Posted.routes.js
+++ routes/Posted.routes.js
@@ -5,7 +5,7 @@ const PostedRouter = express.Router();
 PostedRouter.get("/", async (req, res) => {
     // const airline = req.query
    let { category,order,page } = req.query;
- let data = await PostedModel.find((category)?{category : category}:{},(srh)?{name:srh}:{})
+ let data = await PostedModel.find((category)?{category : category}:{})
     .sort({
     postedAt: order == "asc" || order == "ASC" ? 1 : -1,
     }).skip((page-1)*4)
