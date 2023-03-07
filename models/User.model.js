daf53b48979fe2d0f9a98e2e18d6fbb2d0d95f5b
Bhrugu Prasad <98230777+BhruguPrasad@users.noreply.github.com>
2023-03-07 13:13:33 +0530

    Update User.model.js

--- models/User.model.js
+++ models/User.model.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 
 const userData = new mongoose.Schema({
+    name:String,
     email:String,
-    password:String,
+    password:String
 })
 
 const UserModel = mongoose.model("user",userData);
