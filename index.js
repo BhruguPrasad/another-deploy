3d33850bd2dbafb91f0229238b23bdc5d8b2c4bf
Bhrugu Prasad <98230777+BhruguPrasad@users.noreply.github.com>
2023-03-07 13:12:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,76 +1,66 @@
 
-const bcrypt = require("bcryptjs/dist/bcrypt")
+const bcrypt = require("bcrypt/bcrypt")
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const randomWords = require('random-words');
 var cors = require('cors')
+
 const UserModel = require("./models/User.model")
 const connection = require("./configs/db");
-
-const app = express();
-app.use(express.json());
+const PostedRouter = require("./routes/Posted.routes");
+const app = express(); 
 app.use(cors())
+const PORT = process.env.PORT || 8080
+app.use(express.json());
 require("dotenv").config()
-const PORT = process.env.PORT || 3000
+
 app.get("/",(req,res)=>{
-    res.send("Hello World")
+    res.send("Hello")
 })
-app.get('/randomword', (req, res) => {
-    const randomWord = randomWords();
-    res.json({word:randomWord});
-});
-app.post("/api/signup",async(req,res) =>{
-    const {email,password,name} = req.body;
-    await bcrypt.hash(password,6,function(err,hash){
+
+app.post("/signup", (req, res) => {
+    const {name, email, password} = req.body;
+    bcrypt.hash(password, 5, async function(err, hash) {
         if(err){
-            return res.send("Signup failure")
+            res.send("Something went wrong")
         }
-        const user = new UserModel({email,password:hash,name})
-        user.save()
-        return res.json({msg : "Signup is successfull"})
-    })
+        const user = new UserModel({
+            name,
+            email,
+            password : hash
+        })
+        try{
+            await user.save()
+            res.json({msg : "Signup is successfull"})
+        }
+        catch(err){
+            console.log(err)
+            res.send("Something wrong")
+        }
+    });
 })
-app.post("/api/login",async(req,res) =>{
-    const {email,password} = req.body;
+
+app.post("/login", async (req, res) => {
+    const {email, password} = req.body;
     const user = await UserModel.findOne({email})
-    if(!user){
-        return res.send("Check credentials")
-    }
-    const haspassword = user.password
-    await bcrypt.compare(password,haspassword,(err,result) =>{
-        if(err) {
-            return res.send("error")
+    const hash = user.password
+    bcrypt.compare(password, hash, function(err, result) {
+        if(err){
+            res.send("Something went wrong")
         }
-        if(result==true){
-            const token = jwt.sign({email:user.email,_id:user._id},process.env.jwt_secret_key)
-            return res.send({message:"Login success",token:token,userId:user._id})
+        if(result){
+            const token = jwt.sign({ userId : user._id }, process.env.jwt_secret_key);
+            res.json({message : "Login Successfull",token})
         }
         else{
-            return res.send("Error Please Check")
+            res.send("Invalid Credentials")
         }
-    })
+    });
 })
-const authenticate = (req,res,next) =>{
-    if(!req.headers.authorization){
-        return res.send("Login First");
-    }
-    const token = req.headers.authorization.split(" ")[1]
-    jwt.verify(token,process.env.jwt_secret_key,(err,decode) =>{
-        if(err){
-            return res.send("Login Please");
-        }
-        next()
-    })
-}
-app.use(authenticate)
-app.get("/dashboard",(req,res)=>{
-    res.json({message:"You can access"})
-})
-
+app.use("/classified",PostedRouter)
 app.listen(PORT,async () =>{
     try{
         await connection
-        console.log("Connect success to db")
+        console.log("Connect Success to db")
     }
     catch(err){
         console.log("connect failure")
